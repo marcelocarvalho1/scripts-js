76f0eb3c47e31e9bc40b0e58b6172c87cd5b5b3b
Marcelo <marceloo.dev@gmail.com>
2024-08-27 20:34:09 -0300

    Adiconado imagens e um exemplo de Onclick

--- script.js
+++ script.js
@@ -17,13 +17,25 @@
 //   console.log( multiply(20, 20));
 //   console.log( multiply(0.5, 3));
 
-//Eventos
-document.querySelector("html").addEventListener("click", function () {
-    alert("Ai! Pare de me cutucar!");
-  });
-  // o codigo acima é o mesmo do abaixo 
-document.querySelector("html").addEventListener("click", () => { // ()=> função anonima ou função de seta
-    alert("Ai! Pare de me cutucar!");
-  });
+// //Eventos
+// document.querySelector("html").addEventListener("click", function () {
+//     alert("Ai! Pare de me cutucar!");
+//   });
+//   // o codigo acima é o mesmo do abaixo 
+// document.querySelector("html").addEventListener("click", () => { // ()=> função anonima ou função de seta
+//     alert("Ai! Pare de me cutucar!");
+//   });
   
   
+
+// Exemplo de Onclick para trocar imagem
+const myImage = document.querySelector("img");
+
+minhaImagem.onclick = () => {
+  const mySrc = minhaImagem.getAttribute("src");
+  if (mySrc === "imagens/gh.png") {
+    minhaImagem.setAttribute("src", "imagens/github.png");
+  } else {
+    minhaImagem.setAttribute("src", "imagens/gh.png");
+  }
+};
